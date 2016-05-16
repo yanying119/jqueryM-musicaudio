feed9c863d97c2caa940ee52dd1b3497bb3cdad1
liuwusong <564163433@qq.com>
2016-05-16 10:14:13 +0800

    删除了不必要的文件。添加了播放方式

--- js/js.js
+++ js/js.js
@@ -1,11 +1,12 @@
 $(document).on("pageinit","#index",function(){
 
 	var setList = {
-
 		musicBox:$("#listview"),
 		musicData:musicLise,
 		newArrayIndex:0,
 		musicIndex:0,
+		isOneMusic:false,
+		isRandomMusic:false,
 		getHTML:function() { //创建音乐列表加入页面
 
 			var musicList = [];
@@ -13,13 +14,11 @@ $(document).on("pageinit","#index",function(){
 			for (var p in this.musicData) {
 
 				var me = this.musicData[p];
-				
 				var _html = '<li><a herf="#"><img src="./img/'+me.img+'.jpg" art="'+me.name+'">'+
 						'<p>歌名：'+me.name+'</p>'+
 						'<p>歌手：'+me.autor+'</p>'+
 						'<p class="ui-li-aside">'+me.times+'</p>'+
 						'</a></li>';
-
 
 				musicList.push(_html);
 
@@ -28,13 +27,13 @@ $(document).on("pageinit","#index",function(){
 			return musicList;
 		},
 		getMucisName:function(){//获得所有音乐的信息
+
 			var musicName = [];
+
 			for (var p in this.musicData) {
 
 				var me = this.musicData[p];
-				
 				musicName.push(me.name);
-
 			}
 			return musicName;
 		},
@@ -43,29 +42,44 @@ $(document).on("pageinit","#index",function(){
 			var newArray = this.getHTML().slice(0,i);
 
 			this.newArrayIndex = newArray.length;
-
 			return newArray;
 		},
 		playMusic:function(i){//播放音乐
+
 			var _src = './music/'+this.getMucisName()[i]+".mp3";
 			$("#audio").attr({src:_src,autoplay:"autoplay"});
-		},
-		autoPlayList:function(){
-			var isPlayEnd = $("#audio").ended,
-				_this = this;
-				
-			if(isPlayEnd){
-				_this.musicIndex = _this.musicIndex+1;
-				_this.playMusic(_this.musicIndex);
+
+			if(this.isOneMusic){
+				this.autoPlayList(i);
+			}else if(this.isRandomMusic){
+				var n = Math.random();
+				var i = Math.round(n*this.newArrayIndex);
+				this.autoPlayList(i);
+			}else{
+				this.autoPlayList(i+1);
 			}
+		},
+		autoPlayList:function(i){
+
+			var isPlayEnd = document.getElementById('audio'),
+				_this = this;
+
+			isPlayEnd.addEventListener("ended",function(){
+				if(i>=_this.newArrayIndex){
+					i=0;
+				}
+				_this.playMusic(i);
+			},false);
 		},
 		appendList:function(i){//动态加载音乐列表
 			var a = this.newArrayIndex,
 				b = this.getHTML().length,
 				_this = this; 
+
 			this.musicBox.html("");
 			this.musicBox.append(this.getNewA(i).join(""));
 			this.musicBox.listview("refresh");
+
 			if (a==b){
 				$("#downmore").html("已经全部加载完")
 			}else{
@@ -78,7 +92,8 @@ $(document).on("pageinit","#index",function(){
 			})
 		},
 		eventBind:function(){ //绑定事件
-			var _this = this; 
+			var _this = this;
+
 			$("#downmore").click(function(){
 				_this.newArrayIndex = _this.newArrayIndex+4;
 				if (_this.newArrayIndex>_this.getHTML().length){
@@ -101,9 +116,10 @@ $(document).on("pageinit","#index",function(){
 
 			$("#next").click(function(){
 				if(_this.musicIndex<(_this.newArrayIndex-1)){
+
 					_this.musicIndex = _this.musicIndex + 1;
-					
 				}else{
+
 					_this.musicIndex = _this.musicIndex;
 					alert("后面没有啦、别点啦");
 				};
@@ -111,7 +127,22 @@ $(document).on("pageinit","#index",function(){
 				_this.playMusic(_this.musicIndex);
 			});
 
-
+			$("#playfun").click(function(){
+				var text = $(this).text();
+				if(text == "列表播放"){
+					$(this).text("单曲循环");
+					_this.isOneMusic = true;
+					_this.isRandomMusic = false;
+				}else if(text == "单曲循环"){
+					$(this).text("随机播放");
+					_this.isOneMusic = false;
+					_this.isRandomMusic = true;
+				}else{
+					$(this).text("列表播放");
+					_this.isOneMusic = false;
+					_this.isRandomMusic = false;
+				}
+			});
 		},
 		init:function(){//初始化方法
 			this.eventBind();
